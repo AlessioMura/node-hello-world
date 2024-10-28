003527b0dbaf22fb853ffa57b94c24d989036b4e
AlessioMura <alessiomura03@gmail.com>
2024-10-28 14:40:54 +0100

    added bonus

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const pin = process.env.PIN;
 
 console.log(pin);
 
-const argument = process.argv.slice(2);
+const argument = process.argv[2];
 
 console.log(argument);
+
+
+if (argument == 'admin' && pin == 666) {
+    console.log('Welcome Admin');
+} else {
+    console.log('Access restricted');
+}
